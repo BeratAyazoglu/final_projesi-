8d3b0d5b7043e299ab61848e39b2bd246bea234c
BeratAyazoglu <beratayazoglu61@gmail.com>
2025-05-20 15:50:56 +0300

    Update app.py

--- stokyonetimi/app.py
+++ stokyonetimi/app.py
@@ -363,10 +363,13 @@ def manage_users():
 # VERİTABANI OLUŞTURMA
 # ----------------------
 
+
 if __name__ == '__main__':
     with app.app_context(): 
         if not os.path.exists('site.db'):
             db.create_all()
             create_admin()
             print("Veritabanı oluşturuldu.")
-    app.run(debug=True)
+    
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
